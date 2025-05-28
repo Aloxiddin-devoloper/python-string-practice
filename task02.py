d5912ab55df8c75bc2163cb4e46075ee69165836
Aloxiddin-devoloper <aloxiddinyogmirov1@gmail.com>
2025-05-28 14:19:28 +0500

    bajarildi

--- task02.py
+++ task02.py
@@ -1,1 +1,10 @@
+import random
 
+name1 = input("isminggizni kiriting:")
+name2 = input("Familyanggizni kiriting:")
+
+b = random.randint(1, 100)
+
+a = f"{name1}.{name2}\n {name2}_{name1}\n {name2[0]}{name1}{b}\n {name1}{name2[0]}"
+
+print(a)
